e59e8d8240763397d7ff8a8687737d1120aa9b5c
Study(ByoungChul) <skyksit@gmail.com>
2020-04-16 16:36:46 +0900

    INIT

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false
 
